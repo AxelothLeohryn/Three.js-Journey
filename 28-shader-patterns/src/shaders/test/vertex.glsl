c5cdf3ee1a6248e32516c3e15a494fd423829688
AxelothLeohryn <alejandro97ss@gmail.com>
2024-07-23 23:27:10 +0200

    completed exercise 28, added perlin pattern animation

---
+++ 28-shader-patterns/src/shaders/test/vertex.glsl
@@ -0,0 +1,10 @@
+// attribute vec2 uv; // Already get it from ShaderMaterial
+
+varying vec2 vUv;
+
+void main()
+{
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+    vUv = uv;
+}
